c48a294571ff330fdb8490740dad3ec0bef9dfb2
Brian Morse <brian.p.morse@gmail.com>
2023-05-25 11:13:19 -0700

    completed api routing for posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const {Post} = require("../../models")
 
-// ---> /api/posts
 
+//gets all posts
 router.get("/all", (req, res) => {
     Post.findAll()
     .then(results => {
@@ -10,5 +10,37 @@ router.get("/all", (req, res) => {
     })
 })
 
+    //creates a new post
+    router.post('/', async (req, res) => {
+        try {
+         const post = await Post.create(req.body);
+         res.status(200).json(post);
+       }catch(err) {
+       res.status(400).json(err);
+       }});
+
+
+      
+
+      //delete post by title
+      router.delete('/:title', async (req, res) => {
+        try {
+          const post = await Post.destroy(
+            {
+              where: {
+                title: req.params.title
+              }
+            }
+          )
+          res.status(200).json(post);
+      
+        } catch(err) {
+          console.log(err);
+          res.status(500).json(err);
+        }
+      });
+          
+
+module.exports = router;
 
 module.exports = router;
