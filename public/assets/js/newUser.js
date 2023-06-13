8e05bb411f559d8fdeade162ff863522654de76f
Brian Morse <brian.p.morse@gmail.com>
2023-06-12 18:08:30 -0700

    I forgot to push before I took a long break so I don't remember what changes I made

--- public/assets/js/newUser.js
+++ public/assets/js/newUser.js
@@ -7,19 +7,19 @@ const email = document.querySelector('#new-username').value.trim();
 const password = document.querySelector('#new-password').value.trim();
 const username = document.querySelector('#new-username').value.trim();
 
-
 const data = {
        name: username, 
        email: email,
        password: password
     }
+    res.status(200).json({ message: "Signup form submitted successfully!" });
+    post (req, res) 
 };
-
 
 const submit = document.querySelector('#newSubmit');
 
 submit.addEventListener('click', function() {
-    signupFormHandler(response)
+    signupFormHandler()
 });
 
 
