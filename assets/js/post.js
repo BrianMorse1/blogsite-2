cfd235c1017021d56d58fb27bd765eb33d0fff44
Brian Morse <brian.p.morse@gmail.com>
2023-07-09 10:26:30 -0700

    functionality for new posts added

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,9 +1,34 @@
 const postFormHandler = (event) => {
     event.preventDefault();
 
-    const 
-}
+    const title = document.querySelector('#postTitle').value;
+    const  text = document.querySelector('#postBody').value;
 
+    const data = {
+        title: title,
+        text: text
+    };
+
+    fetch('./api/posts',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(alert('User successfully created! Please navigate to the login page and log in')
+    )
+    .catch(error => {
+        console.log('Error:', error);
+    });
+};
+
+const submit = document.querySelector('#postSubmit');
+
+submit.addEventListener('click', function(event) {
+    postFormHandler(event);
+});
 
 
 
